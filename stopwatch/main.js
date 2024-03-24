9f092d4efb6bae0a7e5cfa150f06ccfa5bce43a9
yrcastillob <yrcastillob@gmail.com>
2024-03-24 16:42:27 -0500

    Stopwatch progression

--- stopwatch/main.js
+++ stopwatch/main.js
@@ -2,6 +2,9 @@
 const hoursHtml = document.getElementById("hours");
 const minutesHtml = document.getElementById("minutes");
 const secondsHtml = document.getElementById("seconds");
+const hoursInputHtml = document.getElementById("hoursInput");
+const minutesInputHtml = document.getElementById("minutesInput");
+const secondsInputHtml = document.getElementById("secondsInput");
 let intervalId = null;
 
 minutesHtml.innerHTML = "0";
@@ -9,31 +12,33 @@ hoursHtml.innerHTML = "0";
 secondsHtml.innerHTML = "0";
 
 function stopWatch() {
-
     let seconds = 0;
     let minutes = 0;
     let hours = 0;
-    minutesHtml.innerHTML = minutes;
-    hoursHtml.innerHTML = hours;
 
-    intervalId = setInterval(() => {
+    secondsHtml.innerHTML = seconds < 10 ? '0' + seconds : seconds;
+    minutesHtml.innerHTML = minutes < 10 ? '0' + minutes : minutes;
+    hoursHtml.innerHTML = hours < 10 ? '0' + hours : hours;
+
+    const intervalId = setInterval(() => {
         seconds++;
-        secondsHtml.innerHTML = seconds;
-        
-        if (seconds === 59) {
+        if (seconds === 60) {
             seconds = 0;
             minutes++;
-            minutesHtml.innerHTML = minutes;
-            if (minutes === 59) {
+            if (minutes === 60) {
                 minutes = 0;
                 hours++;
-                hoursHtml.innerHTML = hours;
-            } 
+            }
         }
+
+        secondsHtml.innerHTML = seconds < 10 ? '0' + seconds : seconds;
+        minutesHtml.innerHTML = minutes < 10 ? '0' + minutes : minutes;
+        hoursHtml.innerHTML = hours < 10 ? '0' + hours : hours;
     }, 1000);
 
     return intervalId;
 }
+
 
 
 function useStopWatch( boolean ){
@@ -45,3 +50,61 @@ function useStopWatch( boolean ){
     }
 }
 
+
+function restart(){
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    secondsHtml.innerHTML = seconds;
+    minutesHtml.innerHTML = minutes;
+    hoursHtml.innerHTML = hours;
+    useStopWatch( false );
+}
+
+function timer() {
+    let seconds = parseInt(secondsInputHtml.value);
+    let minutes = parseInt(minutesInputHtml.value);
+    let hours = parseInt(hoursInputHtml.value);
+    
+    if (isNaN(seconds) || seconds === "" || seconds === null || seconds === undefined) {
+        seconds = 0;
+    }
+
+    if (isNaN(minutes) || minutes === "" || minutes === null || minutes === undefined) {
+        minutes = 0;
+    }
+
+    if (isNaN(hours) || hours === "" || hours === null || hours === undefined) {
+        hours = 0;
+    }
+    secondsHtml.innerHTML = seconds < 10 ? '0' + seconds : seconds;
+    minutesHtml.innerHTML = minutes < 10 ? '0' + minutes : minutes;
+    hoursHtml.innerHTML = hours < 10 ? '0' + hours : hours;
+    
+    if ( hours != 0 || seconds != 0 || minutes != 0 ){
+        const intervalId = setInterval(() => {
+            seconds--;
+    
+            if (seconds < 0) {
+                seconds = 59;
+                minutes--;
+                if (minutes < 0) {
+                    minutes = 59;
+                    hours--;
+                }
+            }
+    
+            secondsHtml.innerHTML = seconds < 10 ? '0' + seconds : seconds;
+            minutesHtml.innerHTML = minutes < 10 ? '0' + minutes : minutes;
+            hoursHtml.innerHTML = hours < 10 ? '0' + hours : hours;
+    
+            if (hours === 0 && minutes === 0 && seconds === 0) {
+                clearInterval(intervalId);
+            }
+        }, 1000);
+    
+        return intervalId;
+    }
+
+    
+}
